3a80929eca18ec09159a17968d121e30e41695a4
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-12-08 02:20:43 -0300

    clean en cada vuelta

--- test/req_test.c
+++ test/req_test.c
@@ -7,7 +7,7 @@ int main(void) {
 
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://matias.insaurral.de/test.html");
 
 //    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
